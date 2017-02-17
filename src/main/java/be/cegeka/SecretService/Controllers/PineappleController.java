06341d037d379ca01b2f848c74cf051612b0b79b
Rodrigo <rodrigo.frazao@cegeka.com>
2017-02-17 17:11:17 +0100

    postman

--- src/main/java/be/cegeka/SecretService/Controllers/PineappleController.java
+++ src/main/java/be/cegeka/SecretService/Controllers/PineappleController.java
@@ -5,6 +5,7 @@ import be.cegeka.SecretService.user.PineappleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
@@ -28,7 +29,10 @@ public class PineappleController {
     @RequestMapping (method = RequestMethod.POST)
     public
     @ResponseBody
-    void AddPinapple(String firstName, String lastName, String username, String password){
+    void AddPinapple(@RequestParam (value = "firstName") String firstName,
+                     @RequestParam (value = "lastName") String lastName,
+                     @RequestParam (value = "username") String username,
+                     @RequestParam (value = "password") String password){
         pineappleService.addPineapple(firstName, lastName, username, password);
     }
 }
